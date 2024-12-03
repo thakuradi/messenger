e946bc51853ea72a13587146b69411db63b47646
Aditya Kumar <84377163+thakuradi@users.noreply.github.com>
2024-12-03 17:30:11 +0530

    update logout

--- messenger/app/hooks/useRoutes.ts
+++ messenger/app/hooks/useRoutes.ts
@@ -25,7 +25,7 @@ const useRoutes = () => {
     {
       label: 'Logout', 
       onClick: () => signOut(),
-      href: '#',
+      href: '/',
       icon: HiArrowLeftOnRectangle, 
     }
   ], [pathname, conversationId]);
